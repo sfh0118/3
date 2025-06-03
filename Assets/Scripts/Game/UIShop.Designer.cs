43f589850b10af3afb68e0cb9c70b9dfa03b5e13
sfh0118 <937125638sun@gmail.com>
2025-06-03 18:26:36 +0900

    v0.3.17 增加通用商店物品& 增加收割购买&出售项目

--- Assets/Scripts/Game/UIShop.Designer.cs
+++ Assets/Scripts/Game/UIShop.Designer.cs
@@ -1,25 +1,25 @@
-// Generate Id:650156aa-afb4-48c4-81c9-c6944a9c8007
+// Generate Id:42add1ca-96c4-4150-8e09-bfc163c85097
 using UnityEngine;
 
 namespace projectlndieFem
 {
 	public partial class UIShop
 	{
-		public UnityEngine.UI.Button BtnBuyChineseCabbageSeed;
+		public UnityEngine.UI.Button BtnTemplate;
 		
-		public UnityEngine.UI.Button BtnBuyRadishSeed;
+		public RectTransform BtnRoot;
 		
 		public UnityEngine.UI.Button BtnBuyFruitSeed;
 		
-		public UnityEngine.UI.Button BtnBuyCarrotSeed;
+		public UnityEngine.UI.Button BtnBuyChineseCabbageSeed;
+		
+		public UnityEngine.UI.Button BtnBuyRadishSeed;
 		
 		public UnityEngine.UI.Button BtnSellFruit;
 		
 		public UnityEngine.UI.Button BtnSellRadish;
 		
 		public UnityEngine.UI.Button BtnSellChineseCabbage;
-		
-		public UnityEngine.UI.Button BtnSellCarrot;
 		
 	}
 }
--- Assets/Scripts/Game/UIShop.cs
+++ Assets/Scripts/Game/UIShop.cs
@@ -23,15 +23,30 @@ namespace projectlndieFem
             }).UnRegisterWhenGameObjectDestroyed(gameObject);
             
         }
-        public static void CreateSellItem(string itemName, Button btn, GameObject gameObject)
+        public  void CreateSellItem(string itemName, string displayName, int sellPrice)
         {
-            if( Global.Interface.GetSystem<IToolBarSystem>().Items.Any(item => item != null && item.Name == itemName))
-            { 
-                btn.Show();
+            var btnSellItem = BtnTemplate.InstantiateWithParent(BtnRoot);
+            btnSellItem.onClick.AddListener(() =>
+            {
+                var toolBarSystem = this.GetSystem<IToolBarSystem>();
+                var carrotItem = toolBarSystem.Items.FirstOrDefault(Item => Item.Name == itemName);
+                if (carrotItem != null)
+                {
+                    Global.Coin.Value += sellPrice;
+                    this.SendCommand(new SubItemCountCommand(itemName, 1));
+                }
+
+
+                AudioController.Get.SfxBuy.Play();
+            });
+            btnSellItem.GetComponentInChildren<Text>().text = displayName;
+            if ( Global.Interface.GetSystem<IToolBarSystem>().Items.Any(item => item != null && item.Name == itemName))
+            {
+                btnSellItem.Show();
             }
             else
             {
-                btn.Hide();
+                btnSellItem.Hide();
             }
             ToolBarSystem.OnItemCountChanged.Register((item, count) =>
             {
@@ -39,27 +54,50 @@ namespace projectlndieFem
                 {
                     if (count >0)
                     {
-                        btn.Show();
+                        btnSellItem.Show();
                     }
                     else
                     {
-                        btn.Hide();
+                        btnSellItem.Hide();
                     }
                 }
             }).UnRegisterWhenGameObjectDestroyed(gameObject);
             
+        }
 
+        public void CreateBuyItem(string itemName, string displayName, int buyPrice)
+        {
+            var btnBuyItem = BtnTemplate.InstantiateWithParent(BtnRoot);
+            btnBuyItem.onClick.AddListener(() =>
+            {
+                Global.Coin.Value -= buyPrice;
+                this.SendCommand(new AddItemCountCommand(itemName, 1));
+
+                AudioController.Get.SfxBuy.Play();
+            });
+            btnBuyItem.GetComponentInChildren<Text>().text = displayName;
+            Global.Coin.RegisterWithInitValue(coin =>
+            {
+                if (coin >= buyPrice)
+                {
+                    btnBuyItem.Show();
+                }
+                else
+                {
+                    btnBuyItem.Hide();
+                }
+            }).UnRegisterWhenGameObjectDestroyed(gameObject);
         }
         void Start()
 		{
+
+            BtnTemplate.Hide();
             SetupBtnShowCheck(Global.FruitCount, BtnSellFruit,(count )=>count >= 1,gameObject);
             SetupBtnShowCheck(Global.RadishCount, BtnSellRadish, (count) => count >= 1, gameObject);
             SetupBtnShowCheck(Global.ChineseCabbageCount, BtnSellChineseCabbage, (count) => count >= 1, gameObject);
-            CreateSellItem("Carrot", BtnSellCarrot, gameObject);
             SetupBtnShowCheck(Global.Coin, BtnBuyFruitSeed, (count) => count >= 1, gameObject);
             SetupBtnShowCheck(Global.Coin, BtnBuyRadishSeed, (count) => count >= 2, gameObject);
             SetupBtnShowCheck(Global.Coin,BtnBuyChineseCabbageSeed, (count) => count >= 3, gameObject);
-            SetupBtnShowCheck(Global.Coin, BtnBuyCarrotSeed, (count) => count >= 4, gameObject);
 
 
 
@@ -87,12 +125,7 @@ namespace projectlndieFem
                 Global.Coin.Value -= 3;
                 AudioController.Get.SfxBuy.Play();
             });
-            BtnBuyCarrotSeed.onClick.AddListener(() =>
-            {
-                this.SendCommand(new AddItemCountCommand("seed_carrot", 1));
-                Global.Coin.Value -= 4;
-                AudioController.Get.SfxBuy.Play();
-            });
+           
 
             BtnSellFruit.onClick.AddListener(() =>
             {
@@ -112,19 +145,21 @@ namespace projectlndieFem
                 Global.ChineseCabbageCount.Value -= 1;
                 AudioController.Get.SfxBuy.Play();
             });
-            BtnSellCarrot.onClick.AddListener(() =>
-            {
-                var toolBarSystem = this.GetSystem<IToolBarSystem>();
-                var carrotItem = toolBarSystem.Items.FirstOrDefault(Item => Item.Name == "carrot");
-                if (carrotItem != null)
-                {
-                    Global.Coin.Value += 12;
-                    this.SendCommand(new SubItemCountCommand("Carrot", 1));
-                }
-               
-                
-                AudioController.Get.SfxBuy.Play();
-            });
+
+
+
+
+            CreateSellItem("carrot", "매출:당근($+12)", 12);
+            CreateSellItem("pumpkin", "매출:호박 ($+20)", 20);
+            CreateSellItem("potato", "매출:감자($10)", 10);
+            CreateSellItem("tomato", "매출:토마토($13)", 13);
+            CreateSellItem("bean", "매출:단콩($15)", 15);
+
+            CreateBuyItem("seed_carot","구입:당근씨앗($-5)", 5);
+            CreateBuyItem("seed_pumpkin", "구입:호박씨앗($-8)", 8);
+            CreateBuyItem("seed_potato", "구입:감자씨앗($-4)", 4);
+            CreateBuyItem("seed_tomato", "구입:토마토씨앗($-5)", 5);
+            CreateBuyItem("seed_bean", "구입:단콩씨앗($-6)", 6);
 
         }
         public IArchitecture GetArchitecture()
